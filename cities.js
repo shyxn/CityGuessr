e8c1dcd6f53dcb6aa420bce2132f8813a90934ae
Morgane Lebre <78199084+shyxn@users.noreply.github.com>
2022-10-07 21:03:36 +0200

    Première version
    Ébauche du jeu
    - Carte
    - 5 villes
    - Affichage des résultats en direct dans la console du navigateur

---
+++ cities.js
@@ -0,0 +1,10 @@
+// Dictionaries are placed in braces, 
+// and values are seperated with a comma
+let citiesCoordinates = {
+	"Paris": [48.862725, 2.287592],
+  	"Marseille": [43.2961743, 5.3699525],
+	"Avignon": [43.9492493,4.8059012],
+    "Niort": [46.3241132,-0.4649403],
+    "Perpignan": [42.6985304,2.8953121],
+    "Lyon": [45.7578137,4.8320114]
+};
